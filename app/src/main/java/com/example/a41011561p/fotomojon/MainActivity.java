e152bf0c914ad193d988921b882738b60f73900c
Killian Herrera Garcia <41011561p@iespoblenou.org>
2019-01-15 18:29:01 +0100

    Dando permisos para acceder a la localizacion(en ello)

--- app/src/main/java/com/example/a41011561p/fotomojon/MainActivity.java
+++ app/src/main/java/com/example/a41011561p/fotomojon/MainActivity.java
@@ -3,32 +3,38 @@ package com.example.a41011561p.fotomojon;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fm = getSupportFragmentManager();
 
-    private TextView mTextMessage;
+    final Fragment fragment1 = new NotificarFragment();
+    final Fragment fragment2 = new LlistarFragment();
+    final Fragment fragment3 = new MapaFragment();
+
+    Fragment active = fragment1;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
+            = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fm.beginTransaction().hide(active).show(fragment1).commit();
+                active = fragment1;
+                return true;
+            case R.id.navigation_llistat:
+                fm.beginTransaction().hide(active).show(fragment2).commit();
+                active = fragment2;
+                return true;
+            case R.id.navigation_mapa:
+                fm.beginTransaction().hide(active).show(fragment3).commit();
+                active = fragment3;
+                return true;
         }
+        return false;
     };
 
     @Override
@@ -36,9 +42,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView nav = findViewById(R.id.navigation);
+        nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fm.beginTransaction()
+                .add(R.id.fragment_seleccionat, fragment1, "1")
+                .hide(fragment1)
+                .commit();
+
+        fm.beginTransaction()
+                .add(R.id.fragment_seleccionat, fragment2, "2")
+                .hide(fragment2)
+                .commit();
+
+        fm.beginTransaction()
+                .add(R.id.fragment_seleccionat, fragment3, "3")
+                .hide(fragment3)
+                .commit();
+
+        nav.setSelectedItemId(R.id.navigation_home);
     }
 
 }
--- app/src/main/java/com/example/a41011561p/fotomojon/NotificarFragment.java
+++ app/src/main/java/com/example/a41011561p/fotomojon/NotificarFragment.java
@@ -1,11 +1,19 @@
 package com.example.a41011561p.fotomojon;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -24,6 +32,38 @@ public class NotificarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notificar, container, false);
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            Log.d(TAG, "getLocation: permissions granted");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                // Si es concedeix permís, obté la ubicació,
+                // d'una altra manera, mostra un Toast
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Toast.makeText(getContext(),
+                            "Permís denegat",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
 }
