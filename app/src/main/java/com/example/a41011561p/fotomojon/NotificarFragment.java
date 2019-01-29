4a6f4837ec50ae066199eaa3ac410dd002701754
Killian Herrera Garcia <41011561p@iespoblenou.org>
2019-01-29 18:29:39 +0100

    Primera parte para hacer busqueda constante ahora solo enseña una loading bar mu flama

--- app/src/main/java/com/example/a41011561p/fotomojon/NotificarFragment.java
+++ app/src/main/java/com/example/a41011561p/fotomojon/NotificarFragment.java
@@ -2,22 +2,34 @@ package com.example.a41011561p.fotomojon;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import static android.content.ContentValues.TAG;
 
@@ -30,9 +42,11 @@ public class NotificarFragment extends Fragment {
     Button buttonLocation;
     final int REQUEST_LOCATION_PERMISSION = 1;
     private final String TAG = this.getClass().getSimpleName();
-    Location mLastLocation;
+    //Location mLastLocation;
     FusedLocationProviderClient mFusedLocationClient;
     TextView mLocationTextView;
+    ProgressBar mLoading;
+    boolean mTrackingLocation;
 
 
     public NotificarFragment() {
@@ -45,11 +59,15 @@ public class NotificarFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_notificar, container, false);
 
         mLocationTextView = view.findViewById(R.id.localitzacio);
-
         buttonLocation = view.findViewById(R.id.button_location);
+        mLoading = view.findViewById(R.id.loading);
 
-        buttonLocation.setOnClickListener(v -> {
-            getLocation();
+        buttonLocation.setOnClickListener((View clickedView) -> {
+            if (!mTrackingLocation) {
+                startTrackingLocation();
+            } else {
+                stopTrackingLocation();
+            }
         });
 
 
@@ -58,9 +76,7 @@ public class NotificarFragment extends Fragment {
         return view;
     }
 
-
-
-    private void getLocation() {
+    private void startTrackingLocation() {
         if (ActivityCompat.checkSelfPermission(this.getContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -71,16 +87,28 @@ public class NotificarFragment extends Fragment {
             mFusedLocationClient.getLastLocation().addOnSuccessListener(
                     location -> {
                         if (location != null) {
-                            mLastLocation = location;
-                            mLocationTextView.setText(
-                                    getString(R.string.location_text,
-                                            mLastLocation.getLatitude(),
-                                            mLastLocation.getLongitude(),
-                                            mLastLocation.getTime()));
+                            // Arranquem l'AsyncTask
+                            new FetchAddressTask(getContext()).execute(location);
                         } else {
                             mLocationTextView.setText("Sense localització coneguda");
                         }
                     });
+        }
+
+        mLocationTextView.setText(getString(R.string.address_text,
+                "Carregant...",
+                System.currentTimeMillis()));
+        mLoading.setVisibility(ProgressBar.VISIBLE);
+        mTrackingLocation = true;
+        buttonLocation.setText("break;");
+
+    }
+
+    private void stopTrackingLocation() {
+        if (mTrackingLocation) {
+            mLoading.setVisibility(ProgressBar.INVISIBLE);
+            mTrackingLocation = false;
+            buttonLocation.setText("while(true) {Get location}");
         }
     }
 
@@ -94,7 +122,7 @@ public class NotificarFragment extends Fragment {
 
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
+                    startTrackingLocation();
                 } else {
                     Toast.makeText(getContext(),
                             "Permís denegat",
@@ -104,4 +132,70 @@ public class NotificarFragment extends Fragment {
         }
     }
 
+    private class FetchAddressTask extends AsyncTask<Location, Void, String> {
+        private final String TAG = FetchAddressTask.class.getSimpleName();
+        private Context mContext;
+
+        FetchAddressTask(Context applicationContext) {
+            mContext = applicationContext;
+        }
+
+        @Override
+        protected String doInBackground(Location... locations) {
+            Geocoder geocoder = new Geocoder(mContext,
+                    Locale.getDefault());
+
+            Location location = locations[0];
+
+            List<Address> addresses = null;
+            String resultMessage = "";
+            try {
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        // En aquest cas, sols volem una única adreça:
+                        1);
+                if (addresses == null || addresses.size() == 0) {
+                    if (resultMessage.isEmpty()) {
+                        resultMessage = "No s'ha trobat cap adreça";
+                        Log.e(TAG, resultMessage);
+                    }
+                } else {
+                    Address address = addresses.get(0);
+                    ArrayList<String> addressParts = new ArrayList<>();
+
+                    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                        addressParts.add(address.getAddressLine(i));
+                    }
+
+                    resultMessage = TextUtils.join("\n", addressParts);
+                }
+            } catch (IOException ioException) {
+                resultMessage = "Servei no disponible";
+                Log.e(TAG, resultMessage, ioException);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                resultMessage = "Coordenades no vàlides";
+                Log.e(TAG, resultMessage + ". " +
+                        "Latitude = " + location.getLatitude() +
+                        ", Longitude = " +
+                        location.getLongitude(), illegalArgumentException);
+            }
+
+            return resultMessage;
+        }
+        @Override
+        protected void onPostExecute(String address) {
+            super.onPostExecute(address);
+            mLocationTextView.setText(getString(R.string.address_text,
+                    address, System.currentTimeMillis()));
+        }
+    }
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
 }
