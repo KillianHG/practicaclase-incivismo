70c34d80b864486970b20cd44a46ca021d443732
Killian Herrera Garcia <41011561p@iespoblenou.org>
2019-02-18 19:33:30 +0100

    Firebase acabado

--- app/src/main/java/com/example/a41011561p/fotomojon/LlistarFragment.java
+++ app/src/main/java/com/example/a41011561p/fotomojon/LlistarFragment.java
@@ -6,6 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -23,7 +31,38 @@ public class LlistarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_llistar, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_llistar, container, false);
 
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        DatabaseReference base = FirebaseDatabase.getInstance().getReference();
+
+        DatabaseReference users = base.child("users");
+        DatabaseReference uid = users.child(auth.getUid());
+        DatabaseReference incidencies = uid.child("incidencies");
+
+
+        FirebaseListOptions<Incidencia> options = new FirebaseListOptions.Builder<Incidencia>()
+                .setQuery(incidencies, Incidencia.class)
+                .setLayout(R.layout.lv_incidencies_item)
+                .setLifecycleOwner(this)
+                .build();
+
+
+        FirebaseListAdapter<Incidencia> adapter = new FirebaseListAdapter<Incidencia>(options) {
+            @Override
+            protected void populateView(View v, Incidencia model, int position) {
+                TextView txtDescripcio = v.findViewById(R.id.txtDescripcio);
+                TextView txtAdreca = v.findViewById(R.id.txtAdreca);
+
+                txtDescripcio.setText(model.getProblema());
+                txtAdreca.setText(model.getDireccio());
+            }
+        };
+
+        ListView lvIncidencies = view.findViewById(R.id.lvIncidencies);
+        lvIncidencies.setAdapter(adapter);
+
+
+        return view;
+    }
 }
