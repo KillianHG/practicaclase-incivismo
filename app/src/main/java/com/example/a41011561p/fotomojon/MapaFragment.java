8dfa195162500f708385eda63d302b5762416855
Killian Herrera Garcia <41011561p@iespoblenou.org>
2019-02-19 18:26:01 +0100

    Mapicas acabado

--- app/src/main/java/com/example/a41011561p/fotomojon/MapaFragment.java
+++ app/src/main/java/com/example/a41011561p/fotomojon/MapaFragment.java
@@ -1,11 +1,31 @@
 package com.example.a41011561p.fotomojon;
 
 
+import android.annotation.SuppressLint;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -19,11 +39,82 @@ public class MapaFragment extends Fragment {
     }
 
 
+    @SuppressLint("MissingPermission")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+        View view = inflater.inflate(R.layout.fragment_mapa, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.g_map);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        DatabaseReference base = FirebaseDatabase.getInstance().getReference();
+
+        if (auth.getUid() != null) {
+            DatabaseReference users = base.child("users");
+            DatabaseReference uid = users.child(auth.getUid());
+            DatabaseReference incidencies = uid.child("incidencies");
+            SharedViewModel model = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+
+
+
+            mapFragment.getMapAsync(map -> {
+                map.setMyLocationEnabled(true);
+                MutableLiveData<LatLng> currentLatLng = model.getCurrentLatLng();
+                LifecycleOwner owner = getViewLifecycleOwner();
+                currentLatLng.observe(owner, latLng -> {
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+                    map.animateCamera(cameraUpdate);
+                    currentLatLng.removeObservers(owner);
+
+                    incidencies.addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                            Incidencia incidencia = dataSnapshot.getValue(Incidencia.class);
+
+                            LatLng aux = new LatLng(
+                                    Double.valueOf(incidencia.getLatitud()),
+                                    Double.valueOf(incidencia.getLongitud())
+                            );
+
+                            IncidenciesInfoWindowAdapter customInfoWindow = new IncidenciesInfoWindowAdapter(
+                                    getActivity()
+                            );
+
+                            Marker marker = map.addMarker(new MarkerOptions()
+                                    .title(incidencia.getProblema())
+                                    .snippet(incidencia.getDireccio())
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                                    .position(aux));
+                            marker.setTag(incidencia);
+                            map.setInfoWindowAdapter(customInfoWindow);
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    });
+                });
+            });
+
+
+
+        }
+        return view;
     }
 
 }
