ed5ca80a87f9e4c22d4fbe82bcce5a6faa869e31
Killian Herrera Garcia <41011561p@iespoblenou.org>
2019-02-25 17:30:04 +0100

    Hacer fotos hecho pero me peta el file_paths.xml

--- app/src/main/java/com/example/a41011561p/fotomojon/NotificarFragment.java
+++ app/src/main/java/com/example/a41011561p/fotomojon/NotificarFragment.java
@@ -2,28 +2,36 @@ package com.example.a41011561p.fotomojon;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -33,8 +41,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -50,11 +61,69 @@ public class NotificarFragment extends Fragment {
     private TextInputEditText txtLongitud;
     private TextInputEditText txtDireccio;
     private TextInputEditText txtDescripcio;
+    private ImageView Foto;
+    private Button buttonFoto;
+
+    String mCurrentPhotoPath;
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private Uri photoURI;
+    private ImageView foto;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(
+                getContext().getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(getContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
 
     private Button buttonNotificar;
     private SharedViewModel model;
 
+
+
     public NotificarFragment() {
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+                Glide.with(this).load(photoURI).into(foto);
+            } else {
+                Toast.makeText(getContext(),
+                        "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -72,6 +141,8 @@ public class NotificarFragment extends Fragment {
         txtDireccio = view.findViewById(R.id.txtDireccio);
         txtDescripcio = view.findViewById(R.id.txtDescripcio);
         buttonNotificar = view.findViewById(R.id.button_notificar);
+        Foto = view.findViewById(R.id.foto);
+        buttonFoto = view.findViewById(R.id.button_foto);
 
         model.getCurrentAddress().observe(this, address -> {
             txtDireccio.setText(getString(R.string.address_text,
@@ -92,6 +163,9 @@ public class NotificarFragment extends Fragment {
 
         model.switchTrackingLocation();
 
+        buttonFoto.setOnClickListener(button -> {
+            dispatchTakePictureIntent();
+        });
 
         buttonNotificar.setOnClickListener(button -> {
             Incidencia incidencia = new Incidencia();
